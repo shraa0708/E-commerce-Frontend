486325e0cf8ff5ce5c5d84b301dec8ffccbbbba0
shraa0708 <84589827+shraa0708@users.noreply.github.com>
2025-05-06 16:01:37 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,43 @@
+const products = [
+  {
+    id: 1,
+    title: "Wireless Headphones",
+    price: 1999,
+    description: "Noise Cancelling over-ear Bluetooth Headphones",
+    category: "Electronics",
+    image: "images/headphones.png",
+  },
+  {
+    id: 2,
+    title: "Smart Watch",
+    price: 2999,
+    description: "Fitness Smart Watch with Heart Rate Monitor",
+    category: "Electronics",
+    image: "images/smartwatch.png",
+  },
+  {
+    id: 3,
+    title: "T-shirt",
+    price: 499,
+    description: "100% Cotton Printed T-shirt",
+    category: "Clothing",
+    image: "images/t-shirt.png",
+  },
+  {
+    id: 4,
+    title: "Laptop",
+    price: 49999,
+    description: "16 GB RAM and 512 SSD Memory with great processor",
+    category:"Electronics",
+    image: "images/laptop.png",
+  },
+  {
+    id: 5,
+    title:"Shoes",
+    price:1599,
+    description:"Comfortable to walk easy to run",
+    category: "Footwear",
+    image:"images/shoes.png"
+    }
+  // Add more products as needed
+];
